3f090c0abfe49c9af253e95527221d063c0d5ba5
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2023-07-18 18:03:30 +0300

    update

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 
-const ul = document.getElementById("ingredients");
+const ul = document.querySelector("#ingredients");
 
-ingredients.forEach(ingredient => {
+const liElements = ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  ul.appendChild(li);
+  return li;
 });
+
+ul.append(...liElements);
